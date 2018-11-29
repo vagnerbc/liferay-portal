1e898a2b91aed4a66c0930f2b459b543c12308c8
Vagner Bernal de castro <vagner@liferay.com>
2018-11-29 11:40:52 -0200

    LPS-86729 journal-test: Add integration test to check the indexed fields for the Journal Article

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/BaseJournalArticleIndexerTestCase.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.search.test;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.search.JournalArticleBlueprint;
+import com.liferay.journal.test.util.search.JournalArticleContent;
+import com.liferay.journal.test.util.search.JournalArticleSearchFixture;
+import com.liferay.journal.test.util.search.JournalArticleTitle;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchEngineHelper;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.search.test.util.IndexedFieldsFixture;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.Inject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vagner B.C
+ */
+public abstract class BaseJournalArticleIndexerTestCase {
+
+	public void setUp() throws Exception {
+		group = GroupTestUtil.addGroup();
+
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		CompanyThreadLocal.setCompanyId(TestPropsValues.getCompanyId());
+
+		_journalArticleSearchFixture = new JournalArticleSearchFixture(
+			journalArticleLocalService);
+
+		_journalArticleSearchFixture.setUp();
+
+		_journalArticles = _journalArticleSearchFixture.getJournalArticles();
+
+		journalArticleIndexerFixture = createJournalArticleIndexerFixture();
+
+		indexedFieldsFixture = createIndexedFieldsFixture();
+	}
+
+	protected IndexedFieldsFixture createIndexedFieldsFixture() {
+		return new IndexedFieldsFixture(
+			resourcePermissionLocalService, searchEngineHelper);
+	}
+
+	protected JournalArticleIndexerFixture createJournalArticleIndexerFixture() {
+		Indexer<JournalArticle> indexer =
+			indexerRegistry.getIndexer(JournalArticle.class);
+
+		return new JournalArticleIndexerFixture(indexer);
+	}
+
+	protected JournalArticle addArticle(String title, String content) {
+		return _journalArticleSearchFixture.addArticle(
+			new JournalArticleBlueprint() {
+				{
+					groupId = group.getGroupId();
+					journalArticleContent = new JournalArticleContent() {
+						{
+							name = "content";
+							defaultLocale = LocaleUtil.US;
+
+							put(LocaleUtil.US, content);
+						}
+					};
+					journalArticleTitle = new JournalArticleTitle() {
+						{
+							put(LocaleUtil.US, title);
+						}
+					};
+				}
+			});
+	}
+
+	protected IndexedFieldsFixture indexedFieldsFixture;
+
+	@Inject
+	protected IndexerRegistry indexerRegistry;
+
+	@Inject
+	protected ResourcePermissionLocalService resourcePermissionLocalService;
+
+	@Inject
+	protected SearchEngineHelper searchEngineHelper;
+
+	protected JournalArticleSearchFixture _journalArticleSearchFixture;
+	protected JournalArticleIndexerFixture journalArticleIndexerFixture;
+
+	@Inject
+	protected JournalArticleLocalService journalArticleLocalService;
+
+	@DeleteAfterTestRun
+	protected Group group;
+
+	@DeleteAfterTestRun
+	private List<JournalArticle> _journalArticles = new ArrayList<>(1);
+}
---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleIndexerFixture.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.search.test;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.search.test.util.HitsAssert;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * @author Vagner B.C
+ */
+public class JournalArticleIndexerFixture {
+
+
+	public JournalArticleIndexerFixture(Indexer<JournalArticle> indexer) {
+		_indexer = indexer;
+	}
+
+	public SearchContext getSearchContext(String keywords, Locale locale)
+		throws Exception {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setCompanyId(TestPropsValues.getCompanyId());
+		searchContext.setKeywords(keywords);
+		searchContext.setLocale(Objects.requireNonNull(locale));
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setSelectedFieldNames(StringPool.STAR);
+
+		return searchContext;
+	}
+
+	public Document searchOnlyOne(String keywords) throws Exception {
+		return searchOnlyOne(keywords, Locale.US);
+	}
+
+	public Document searchOnlyOne(String keywords, Locale locale)
+		throws Exception {
+
+		Hits search = search(getSearchContext(keywords, locale));
+
+		return HitsAssert.assertOnlyOne(search);
+	}
+
+	public void searchNoOne(String keywords, Locale locale) throws Exception {
+		HitsAssert.assertNoHits(search(getSearchContext(keywords, locale)));
+	}
+
+	protected long getUserId() throws Exception {
+		return TestPropsValues.getUserId();
+	}
+
+	protected void reindex(long companyId) throws Exception {
+		String[] ids = {String.valueOf(companyId)};
+
+		_indexer.reindex(ids);
+	}
+
+	protected Hits search(SearchContext searchContext) throws Exception {
+		return _indexer.search(searchContext);
+	}
+
+	public void deleteDocument(Document document)
+		throws PortalException, SearchException {
+		IndexWriterHelperUtil.deleteDocument(
+			_indexer.getSearchEngineId(), TestPropsValues.getCompanyId(),
+			document.getUID(), true);
+	}
+
+	private final Indexer<JournalArticle> _indexer;
+}
