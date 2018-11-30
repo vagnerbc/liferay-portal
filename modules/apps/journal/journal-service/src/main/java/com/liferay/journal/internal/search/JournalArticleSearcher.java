13a5e8c8ae128cb7ac2c1fcf842bf029b23fb255
Vagner Bernal de castro <vagner@liferay.com>
2018-11-30 13:13:38 -0200

    LPS-86729 journal-service: Integrate JournalArticle to new indexer model

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleSearcher.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.search;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.search.Field;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @author Harry Mark
+ * @author Bruno Farache
+ * @author Raymond Augé
+ * @author Hugo Huijser
+ * @author Tibor Lipusz
+ * @author Vagner B.C
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.journal.model.JournalArticle",
+	service = BaseSearcher.class
+)
+public class JournalArticleSearcher extends BaseSearcher {
+
+	public static final String CLASS_NAME = JournalArticle.class.getName();
+
+	public JournalArticleSearcher() {
+		setDefaultSelectedFieldNames(
+			Field.ASSET_TAG_NAMES, Field.ARTICLE_ID, Field.COMPANY_ID,
+			Field.DEFAULT_LANGUAGE_ID, Field.ENTRY_CLASS_NAME,
+			Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE,
+			Field.SCOPE_GROUP_ID, Field.VERSION, Field.UID);
+
+		setDefaultSelectedLocalizedFieldNames(
+			Field.CONTENT, Field.DESCRIPTION, Field.TITLE);
+
+		setFilterSearch(true);
+		setPermissionAware(true);
+		setSelectAllLocales(true);
+	}
+
+	@Override
+	public String getClassName() {
+		return CLASS_NAME;
+	}
+
+}
