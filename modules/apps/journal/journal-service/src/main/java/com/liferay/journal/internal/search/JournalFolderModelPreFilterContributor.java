21ce362aaf939c34109599430111f98ac4a9c823
Vagner Bernal de castro <vagner@liferay.com>
2018-12-04 13:43:36 -0200

    LPS-86728 journal-service: Integrate JournalFolder to new indexer model

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderModelPreFilterContributor.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderModelPreFilterContributor.java
@@ -1,4 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.journal.internal.search;
 
-public class JournalFolderModelPreFilterContributor {
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eduardo Garcia
+ * @author Vagner B.C
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.journal.model.JournalFolder",
+	service = ModelPreFilterContributor.class
+)
+public class JournalFolderModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter contextBooleanFilter,
+		ModelSearchSettings modelSearchSettings, SearchContext searchContext) {
+
+		try {
+			addStatus(contextBooleanFilter, searchContext);
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to execute JournalFolderModelPreFilterContributor");
+			}
+		}
+	}
+
+	protected void addStatus(
+			BooleanFilter contextBooleanFilter, SearchContext searchContext)
+		throws Exception {
+
+		int[] statuses = GetterUtil.getIntegerValues(
+			searchContext.getAttribute(Field.STATUS), null);
+
+		if (ArrayUtil.isEmpty(statuses)) {
+			int status = GetterUtil.getInteger(
+				searchContext.getAttribute(Field.STATUS),
+				WorkflowConstants.STATUS_APPROVED);
+
+			statuses = new int[] {status};
+		}
+
+		if (!ArrayUtil.contains(statuses, WorkflowConstants.STATUS_ANY)) {
+			TermsFilter statusesTermsFilter = new TermsFilter(Field.STATUS);
+
+			statusesTermsFilter.addValues(ArrayUtil.toStringArray(statuses));
+
+			contextBooleanFilter.add(
+				statusesTermsFilter, BooleanClauseOccur.MUST);
+		}
+		else {
+			contextBooleanFilter.addTerm(
+				Field.STATUS, String.valueOf(WorkflowConstants.STATUS_IN_TRASH),
+				BooleanClauseOccur.MUST_NOT);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalFolderModelPreFilterContributor.class);
+
 }
