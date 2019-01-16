1bf6b685922ad0336faf88a1aeb5d3d68672682a
Vagner Bernal de castro <vagner@liferay.com>
2019-01-16 09:59:52 -0200

    LPS-86736 organization-test: Adjust Organization search test

--- modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/search/test/OrganizationIndexerReindexTest.java
+++ modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/search/test/OrganizationIndexerReindexTest.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.search.test.util.IndexerFixture;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -75,7 +76,8 @@ public class OrganizationIndexerReindexTest {
 		Document document = organizationIndexerFixture.searchOnlyOne(
 			organizationName);
 
-		organizationIndexerFixture.deleteDocument(document);
+		organizationIndexerFixture.deleteDocument(
+			document, TestPropsValues.getCompanyId());
 
 		organizationIndexerFixture.searchNoOne(organizationName);
 
