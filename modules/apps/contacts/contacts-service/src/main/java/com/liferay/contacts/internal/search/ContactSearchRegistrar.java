c7b64663e1b663ee914a49070b9b7e7775aed9af
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-07-22 15:06:46 -0300

    LPS-97798 contacts-service: Inject ModelDocumentContributor on Registrar class

--- modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/search/ContactSearchRegistrar.java
+++ modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/internal/search/ContactSearchRegistrar.java
@@ -15,11 +15,15 @@
 package com.liferay.contacts.internal.search;
 
 import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchDefinition;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchRegistrarHelper;
 import com.liferay.portal.search.spi.model.registrar.contributor.ModelSearchDefinitionContributor;
 import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -27,6 +31,8 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Lucas Marques de Paula
@@ -46,6 +52,8 @@ public class ContactSearchRegistrar {
 
 					modelSearchDefinition.setModelIndexWriteContributor(
 						modelIndexWriterContributor);
+					modelSearchDefinition.setModelDocumentContributors(
+						modelDocumentContributors);
 					modelSearchDefinition.setModelSummaryContributor(
 						modelSummaryContributor);
 				}
@@ -53,10 +61,30 @@ public class ContactSearchRegistrar {
 			});
 	}
 
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(indexer.class.name=com.liferay.portal.kernel.model.Contact)"
+	)
+	protected void addModelDocumentContributor(
+		ModelDocumentContributor modelDocumentContributor) {
+
+		modelDocumentContributors.add(modelDocumentContributor);
+	}
+
 	@Deactivate
 	protected void deactivate() {
 		_serviceRegistration.unregister();
 	}
+
+	protected void removeModelDocumentContributor(
+		ModelDocumentContributor modelDocumentContributor) {
+
+		modelDocumentContributors.remove(modelDocumentContributor);
+	}
+
+	protected List<ModelDocumentContributor> modelDocumentContributors =
+		new ArrayList<>();
 
 	@Reference(
 		target = "(indexer.class.name=com.liferay.portal.kernel.model.Contact)"
