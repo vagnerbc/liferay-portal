583dc3dbf101be8bc932f8fa9b7b4e4c4bbcaefd
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-07-22 15:11:17 -0300

    LPS-97798 document-library-service: Inject ModelDocumentContributor on Registrar class

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/DLFileEntrySearchRegistrar.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/DLFileEntrySearchRegistrar.java
@@ -16,10 +16,14 @@ package com.liferay.document.library.internal.search;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchRegistrarHelper;
 import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
 import com.liferay.portal.search.spi.model.result.contributor.ModelVisibilityContributor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -27,6 +31,8 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Michael C. Han
@@ -46,6 +52,8 @@ public class DLFileEntrySearchRegistrar {
 					Field.TITLE, Field.UID);
 				modelSearchDefinition.setModelIndexWriteContributor(
 					modelIndexWriterContributor);
+				modelSearchDefinition.setModelDocumentContributors(
+					modelDocumentContributors);
 				modelSearchDefinition.setModelVisibilityContributor(
 					modelVisibilityContributor);
 				modelSearchDefinition.setModelSummaryContributor(
@@ -53,10 +61,30 @@ public class DLFileEntrySearchRegistrar {
 			});
 	}
 
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(indexer.class.name=com.liferay.document.library.kernel.model.DLFileEntry)"
+	)
+	protected void addModelDocumentContributor(
+		ModelDocumentContributor modelDocumentContributor) {
+
+		modelDocumentContributors.add(modelDocumentContributor);
+	}
+
 	@Deactivate
 	protected void deactivate() {
 		_serviceRegistration.unregister();
 	}
+
+	protected void removeModelDocumentContributor(
+		ModelDocumentContributor modelDocumentContributor) {
+
+		modelDocumentContributors.remove(modelDocumentContributor);
+	}
+
+	protected List<ModelDocumentContributor> modelDocumentContributors =
+		new ArrayList<>();
 
 	@Reference(
 		target = "(indexer.class.name=com.liferay.document.library.kernel.model.DLFileEntry)"
