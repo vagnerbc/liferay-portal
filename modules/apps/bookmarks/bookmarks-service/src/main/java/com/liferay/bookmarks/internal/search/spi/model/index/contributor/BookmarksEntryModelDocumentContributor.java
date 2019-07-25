7de5dfede32749d6671d0475df95616bf1e41946
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-07-25 15:05:52 -0300

    LPS-99311 bookmarks-service: Add multilanguage support

--- modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/search/spi/model/index/contributor/BookmarksEntryModelDocumentContributor.java
+++ modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/search/spi/model/index/contributor/BookmarksEntryModelDocumentContributor.java
@@ -16,10 +16,15 @@ package com.liferay.bookmarks.internal.search.spi.model.index.contributor;
 
 import com.liferay.bookmarks.model.BookmarksEntry;
 import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -43,6 +48,27 @@ public class BookmarksEntryModelDocumentContributor
 			Field.TREE_PATH,
 			StringUtil.split(bookmarksEntry.getTreePath(), CharPool.SLASH));
 		document.addText(Field.URL, bookmarksEntry.getUrl());
+
+		_addLocalizedFields(document, bookmarksEntry);
+	}
+
+	private void _addLocalizedFields(
+		Document document, BookmarksEntry bookmarksEntry) {
+
+		for (Locale locale :
+				LanguageUtil.getAvailableLocales(bookmarksEntry.getGroupId())) {
+
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
+				bookmarksEntry.getName());
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(
+					Field.DESCRIPTION, languageId),
+				bookmarksEntry.getDescription());
+		}
 	}
 
 }
--- modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/search/spi/model/index/contributor/BookmarksFolderModelDocumentContributor.java
+++ modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/search/spi/model/index/contributor/BookmarksFolderModelDocumentContributor.java
@@ -16,10 +16,15 @@ package com.liferay.bookmarks.internal.search.spi.model.index.contributor;
 
 import com.liferay.bookmarks.model.BookmarksFolder;
 import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -43,6 +48,28 @@ public class BookmarksFolderModelDocumentContributor
 		document.addKeyword(
 			Field.TREE_PATH,
 			StringUtil.split(bookmarksFolder.getTreePath(), CharPool.SLASH));
+
+		_addLocalizedFields(document, bookmarksFolder);
+	}
+
+	private void _addLocalizedFields(
+		Document document, BookmarksFolder bookmarksFolder) {
+
+		for (Locale locale :
+				LanguageUtil.getAvailableLocales(
+					bookmarksFolder.getGroupId())) {
+
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
+				bookmarksFolder.getName());
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(
+					Field.DESCRIPTION, languageId),
+				bookmarksFolder.getDescription());
+		}
 	}
 
 }
