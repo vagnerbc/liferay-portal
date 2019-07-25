eb4582ac4fef5004e5ac0b50dbc0f4807d456631
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-07-25 17:13:39 -0300

    LPS-99314 user-groups-admin-impl: Add multilanguage support

--- modules/apps/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/search/spi/model/index/contributor/UserGroupModelDocumentContributor.java
+++ modules/apps/user-groups-admin/user-groups-admin-impl/src/main/java/com/liferay/user/groups/admin/internal/search/spi/model/index/contributor/UserGroupModelDocumentContributor.java
@@ -14,10 +14,17 @@
 
 package com.liferay.user.groups.admin.internal.search.spi.model.index.contributor;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -38,6 +45,31 @@ public class UserGroupModelDocumentContributor
 		document.addText(Field.DESCRIPTION, userGroup.getDescription());
 		document.addText(Field.NAME, userGroup.getName());
 		document.addKeyword(Field.USER_GROUP_ID, userGroup.getUserGroupId());
+
+		_addLocalizedFields(document, userGroup);
 	}
+
+	private void _addLocalizedFields(Document document, UserGroup userGroup) {
+		try {
+			for (Locale locale :
+					LanguageUtil.getAvailableLocales(userGroup.getGroupId())) {
+
+				String languageId = LocaleUtil.toLanguageId(locale);
+
+				document.addText(
+					LocalizationUtil.getLocalizedName(
+						Field.DESCRIPTION, languageId),
+					userGroup.getDescription());
+			}
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(e, e);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UserGroupModelDocumentContributor.class);
 
 }
