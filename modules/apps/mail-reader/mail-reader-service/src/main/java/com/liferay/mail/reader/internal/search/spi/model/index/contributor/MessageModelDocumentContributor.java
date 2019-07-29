8f6d2d8eecabfb6e53d4ce01822dfc518668ffeb
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-07-29 11:28:08 -0300

    LPS-97799 mail-reader-service: Add multilanguage support

--- modules/apps/mail-reader/mail-reader-service/src/main/java/com/liferay/mail/reader/internal/search/spi/model/index/contributor/MessageModelDocumentContributor.java
+++ modules/apps/mail-reader/mail-reader-service/src/main/java/com/liferay/mail/reader/internal/search/spi/model/index/contributor/MessageModelDocumentContributor.java
@@ -17,10 +17,17 @@ package com.liferay.mail.reader.internal.search.spi.model.index.contributor;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.expando.kernel.util.ExpandoBridgeIndexerUtil;
 import com.liferay.mail.reader.model.Message;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -50,6 +57,35 @@ public class MessageModelDocumentContributor
 		document.addKeyword("remoteMessageId", message.getRemoteMessageId());
 
 		ExpandoBridgeIndexerUtil.addAttributes(document, expandoBridge);
+
+		_addLocalizedFields(document, message);
 	}
+
+	private void _addLocalizedFields(Document document, Message message) {
+		try {
+			for (Locale locale :
+					LanguageUtil.getAvailableLocales(message.getGroupId())) {
+
+				String languageId = LocaleUtil.toLanguageId(locale);
+
+				document.addText(
+					LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
+					message.getSubject());
+
+				document.addText(
+					LocalizationUtil.getLocalizedName(
+						Field.CONTENT, languageId),
+					HtmlUtil.extractText(message.getBody()));
+			}
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(e, e);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MessageModelDocumentContributor.class);
 
 }
